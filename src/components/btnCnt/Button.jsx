caa3603f58dcc4fbd349f5467ef5f34b8370d937
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-21 16:14:18 +0530

    style added

--- src/components/btnCnt/Button.jsx
+++ src/components/btnCnt/Button.jsx
@@ -4,11 +4,22 @@ import { globalContext } from '../../dataContext/DataProvider';
 import './btnStyle.css';
 
 const Button = ({ item }) => {
-  const { controlNum, detectSign, evaluteFunction, resetFunction, cancelBtn } =
-    useContext(globalContext);
+  const {
+    controlNum,
+    detectSign,
+    evaluteFunction,
+    resetFunction,
+    cancelBtn,
+    isActive,
+  } = useContext(globalContext);
 
   return (
     <button
+      id={
+        isActive && item != '=' && item != 'RESET'
+          ? 'btnOff'
+          : item != '=' && item != 'RESET'
+      }
       className={item == '=' ? 'equals' : item == 'RESET' ? 'reset' : 'num-btn'}
       onClick={() => {
         item == '+' || item == '-' || item == 'x' || item == '/'
